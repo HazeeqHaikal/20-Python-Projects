751ebd119ba773e96270ff7714d27adabd7a0c2b
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-14 19:19:43 +0800

    finished word replacement

---
+++ wordReplacement.py
@@ -0,0 +1,8 @@
+def replaceWord():
+    str = "Woi okay ka tu, hi hi hi hi"
+    wordToReplace = input("Enter the word to replace: ")
+    wordReplacement = input("Enter the word replacement: ")
+    print(str.replace(wordToReplace, wordReplacement))
+
+replaceWord()
+    
