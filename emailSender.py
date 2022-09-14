8736d43dd7b7396481c2d3bf837fb0c464f957d6
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-14 19:05:08 +0800

    finished emailsender

--- emailSender.py
+++ emailSender.py
@@ -1,7 +1,28 @@
 from email.message import  EmailMessage
 from accountDetail import *
+import ssl
+import smtplib
 
 emailSender = email
 emailPassword = password
 
-emailReceiver = ''
+emailReceiver = receiver
+
+subject = "Hi testing sending email"
+body = """
+Is this thing on?
+"""
+
+em = EmailMessage()
+em['From'] = emailSender
+em['To'] = emailReceiver
+em['Subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+    smtp.login(emailSender, emailPassword)
+    smtp.sendmail(emailSender, emailReceiver, em.as_string())
+
+print("Finished sending email")
