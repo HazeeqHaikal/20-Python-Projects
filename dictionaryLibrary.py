ce996bdcf0a0e83775ae83bd85f6d26c8a8c80a3
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-25 00:32:23 +0800

    update dictionary library

--- dictionaryLibrary.py
+++ dictionaryLibrary.py
@@ -5,17 +5,22 @@ dictionary=PyDictionary()
 
 def translate():
     
-    word = input("Enter your word: ").capitalize()
+    word = input("Enter your word: ")
     t = PrettyTable([word, 'Meaning'])
-    meaning = dictionary.meaning(word)
     
-    count = 1
+    try:
+        word = word.capitalize()
+        meaning = dictionary.meaning(word)
+        count = 1
     
-    for x in meaning:
-        for i in meaning[x]:
-            t.add_row([count, i])
-            count += 1
-    print(t)
+        for x in meaning:
+            for i in meaning[x]:
+                t.add_row([count, i])
+                count += 1
+        print(t)
+    except:
+        print("Word not found in dictionary")
+        translate()
     
     
 if __name__ == '__main__':
