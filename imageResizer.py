62befa12408a89df86fd2398843b8aff3ce4f7a0
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-25 17:53:54 +0800

    update image resizer

--- imageResizer.py
+++ imageResizer.py
@@ -1,10 +1,17 @@
 from PIL import Image
 
-image = Image.open('./image/cr7.png')
+def resizeImage(size1, size2):
+    
+    image = Image.open('./image/cr7.png')
 
-print(f"Current image size: {image.size}")
+    print(f"Current image size: {image.size}")
 
-resizeImage = image.resize((300, 300))
+    resizeImage = image.resize((size1, size2))
 
-resizeImage.save('./image/cr7_resized.png')
-print("Image resized successfully")
+    resizeImage.save('./image/cr7_resized.png')
+    print("Image resized successfully")
+
+if __name__ == '__main__':
+    size1 = int(input("Enter the width of the image: "))
+    size2 = int(input("Enter the height of the image: "))
+    resizeImage(size1, size2)
