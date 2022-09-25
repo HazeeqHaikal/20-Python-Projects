10ac4fc165d28f536458ebf28ee2738120a7946f
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-25 12:05:36 +0800

    minor update 2.0

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -3,6 +3,8 @@ import sys
 
 def rps():
     options = ["rock", "paper", "scissors"]
+    userPoints = 0
+    computerPoints = 0
     print("Welcome to Rock, Paper, Scissors!")
     print("Enter your choice of rock (r), paper (p), or scissors (s).")
     print("Enter 'q' to quit.")
@@ -11,7 +13,14 @@ def rps():
         if user == "r" or user == "p" or user == "s":
             user = user.replace("r", "rock").replace("p", "paper").replace("s", "scissors")
         if user == 'q':
-            print("Thanks for playing!")
+            print("")
+            if userPoints > computerPoints:
+                print("You won!")
+            elif userPoints < computerPoints:
+                print("Computer won!")
+            else:
+                print("You and computer got a tie!")
+            print("Thanks for playing! Your last score is {} and computer is {}.".format(userPoints, computerPoints))
             sys.exit()
         elif user == 'rock' or user == 'paper' or user == 'scissors':
             computer = random.choice(options)
@@ -20,12 +29,16 @@ def rps():
                 print("It's a tie!")
             elif user == 'rock' and computer == 'scissors':
                 print("You win!")
+                userPoints += 1
             elif user == 'paper' and computer == 'rock':
                 print("You win!")
+                userPoints += 1
             elif user == 'scissors' and computer == 'paper':
                 print("You win!")
+                userPoints += 1
             else:
                 print("You lose!")
+                computerPoints += 1
         else:
             print("You did not enter a valid choice.")
 
