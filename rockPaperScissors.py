bfb806b0cfc63b499e082b8195c488b1c4bf985b
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-09-25 11:53:23 +0800

    rock paper scissors

---
+++ rockPaperScissors.py
@@ -0,0 +1,32 @@
+import random
+import sys
+
+def rps():
+    options = ["rock", "paper", "scissors"]
+    print("Welcome to Rock, Paper, Scissors!")
+    print("Enter your choice of rock (r), paper (p), or scissors (s).")
+    print("Enter 'q' to quit.")
+    while True:
+        user = input("Enter your choice: ").lower()
+        if user == "r" or user == "p" or user == "s":
+            user = user.replace("r", "rock").replace("p", "paper").replace("s", "scissors")
+        if user == 'q':
+            sys.exit()
+        elif user == 'rock' or user == 'paper' or user == 'scissors':
+            computer = random.choice(options)
+            print("You chose " + user + " and the computer chose " + computer + ".")
+            if user == computer:
+                print("It's a tie!")
+            elif user == 'rock' and computer == 'scissors':
+                print("You win!")
+            elif user == 'paper' and computer == 'rock':
+                print("You win!")
+            elif user == 'scissors' and computer == 'paper':
+                print("You win!")
+            else:
+                print("You lose!")
+        else:
+            print("You did not enter a valid choice.")
+
+if __name__ == '__main__':
+    rps()
